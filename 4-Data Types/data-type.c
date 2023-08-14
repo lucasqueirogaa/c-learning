38e6acd0e68e9c6158768d4d3136b870d2f7f054
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-08-14 08:47:31 -0300

    Learning about data-type

---
+++ 4-Data Types/data-type.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  // Int, for integer numbers
+  int integer = 40;
+  // Double, for decimal numbers and numbers like "40.0"
+  double decial = 40.67;
+  // Char, for characters, we will use with "chat nameOfVar" only one letter, for a string, we can use like this: "chat nameOfVar[]"
+  char character = 'A';
+  // String, a variable from char to use more characters. Is like a array of characters
+  char phrase[] = 'Name Here';
+
+  return 0;
+
+}
