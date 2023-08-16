b5ff2f57a4d47f4888c458c09535921f0897afa1
Lucas Queiroga <lucasqueirogadev@gmail.com>
2023-08-16 09:11:38 -0300

    Learning about memory addresses

---
+++ 20 - Memory Addresses/memory-address.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  int age = 30;
+  double gpa = 3.4;
+  char grade = 'A';
+
+  printf("Age: %p\nGpa: %p\nGrade: %p\n", &age, &gpa, &grade);
+
+  return 0;
+
+}
